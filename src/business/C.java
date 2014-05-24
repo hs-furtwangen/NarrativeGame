b1a991e1a46b991b2a2bff1cbb2c2accc778ae10
kuhnertj <jens.kuhnert@hs-furtwangen.de>
2014-05-24 04:22:50 +0200

    different scenes. posX and posY will reset if you go into a scene the second time

--- src/business/C.java
+++ src/business/C.java
@@ -4,19 +4,19 @@ public abstract class C {
 	
 	public static int TILE_WIDTH = 16;
 	public static int TILE_HEIGHT = 16;
-	public static int SCREEN_WIDTH = 600;
-	public static int SCREEN_HEIGHT = 400;
+	public static int SCREEN_WIDTH = 800;
+	public static int SCREEN_HEIGHT = 600;
 	public static int FPS = 30;
 	public static int STEPWIDTH = 2;
 	public static int JUMP_HEIGHT = 80;
 	
 	public static String PATH = "ressources/images/";
 	public static String PASS_MAP = "ressources/maps/pass_map.png";
-	public static String IMG_MAP = "ressources/maps/map.png";
+	public static String IMG_MAP = "ressources/maps/pass_map.png";
 	public static int MAP_WIDTH = 10;
 	public static int MAP_HEIGHT = 5;
 	
 	public static char NONPASSABLE = 'x';
-	public static int ANIMATIONTEMPO = 6;
+	public static int ANIMATIONTEMPO = 8;
 
 }
